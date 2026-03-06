c52f54ae88ea7df9d78924611f21580f28138dcd
dunmomee-blip <raafiah.oladunmomi@gmail.com>
2026-03-06 05:24:50 -0800

    injected weather data

--- src/app.js
+++ src/app.js
@@ -4,6 +4,41 @@ function displayWeather(response) {
   let tempElement = document.querySelector("#weather-temp");
   let temperature = Math.round(response.data.temperature.current);
   tempElement.innerHTML = Math.round(temperature);
+  let descElement = document.querySelector("#cloud-desc");
+  descElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCityTemp(city) {
